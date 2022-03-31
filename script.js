701fe342ecb4632bd6e148a52e72e98fb2f3fd87
Jesse Marino <jessemarino2020@gmail.com>
2022-03-31 09:51:42 -0700

    code generates a random password after adding if statments

--- script.js
+++ script.js
@@ -5,26 +5,23 @@ function generatePassword() {
   var password = "";
   // TODO: add code to generate the password here
   var charnumber = window.prompt("Enter how many characters 8-120");
+    if (!charnumber || charnumber < 8 || charnumber > 120) {return;}
   var lowercase = window.confirm("Would you like lowercase?");
   var uppercase = window.confirm("Would you like uppercase?");
   var number = window.confirm("Would you like numbers?");
   var special = window.confirm("Would you like special characters?");
-  var randochar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*&*(/?";
-
+  var randochar = "";
+    if (lowercase == true) {randochar = randochar + "abcdefghijklmnopqrstuvwxyz"};
+    if (uppercase == true) {randochar = randochar + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    if (number == true) {randochar = randochar + "0","1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    if (special == true) {randochar = randochar + "!@#$%^&*(/?"};
   
   for (var i = 0; i < charnumber; i++) {
-    password += randochar.charAt(Math.floor(Math.random() * randochar.length));
+    password += randochar.charAt(Math.floor(Math.random() * randochar.length));}
   
 
-  
-  }
   return password;
-
-
-
-
-
-}
+  }
 
 // Write password to the #password input
 function writePassword() {
