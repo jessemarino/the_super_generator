d775331c681e08fd38106510d65f8dd6d500a07b
Jesse Marino <jessemarino2020@gmail.com>
2022-03-30 23:15:00 -0700

    added random characters and for functinon

--- script.js
+++ script.js
@@ -2,17 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var password = "password";
+  var password = "";
   // TODO: add code to generate the password here
-  var length = window.prompt("Enter how many characters 8-120") 
-  var lowercase = window.confirm("Would you like lowercase?")
-  var uppercase = window.confirm("Would you like uppercase?")
-  var number = window.confirm("Would you like numbers?")
-  var special = window.confirm("Would you like special characters?")
+  var charnumber = window.prompt("Enter how many characters 8-120");
+  var lowercase = window.confirm("Would you like lowercase?");
+  var uppercase = window.confirm("Would you like uppercase?");
+  var number = window.confirm("Would you like numbers?");
+  var special = window.confirm("Would you like special characters?");
+  var randochar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*&*(/?";
+
   
+  for (var i = 0; i < charnumber; i++) {
+    password += randochar.charAt(Math.floor(Math.random() * randochar.length));
   
+
   
+  }
   return password;
+
+
+
+
+
 }
 
 // Write password to the #password input
