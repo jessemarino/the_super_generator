a5fe53f3d70a3cbf4cf7ef41b13518ba8d9a5cde
Jesse Marino <jessemarino2020@gmail.com>
2022-03-30 22:41:03 -0700

    added prompts and confirms for window

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   var password = "password";
   // TODO: add code to generate the password here
-
+  var length = window.prompt("Enter how many characters 8-120") 
+  var lowercase = window.confirm("Would you like lowercase?")
+  var uppercase = window.confirm("Would you like uppercase?")
+  var number = window.confirm("Would you like numbers?")
+  var special = window.confirm("Would you like special characters?")
+  
+  
+  
   return password;
 }
 
